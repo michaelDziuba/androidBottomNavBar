f6154d1657d81ea7e823c2dc660ddbf490eae70f
Michael Dziuba <michael.dziuba@stclairconnect.ca>
2018-06-06 18:36:29 -0400

    Renamed project SplashScreen

--- MedsCalculator/app/src/main/java/com/readablecolors/mdziuba/medscalculator/AppFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.readablecolors.mdziuba.medscalculator
-import android.content.SharedPreferences
-
-interface AppFragment {
-
-    var fragmentId: Int
-}
---
+++ SplashScreen/app/src/main/java/com/readablecolors/mdziuba/splashscreen/AppFragment.kt
@@ -0,0 +1,6 @@
+package com.readablecolors.mdziuba.splashscreen
+
+interface AppFragment {
+
+    var fragmentId: Int
+}
